eeed80dcd4453f13e9df27796e4f73b66ad042f4
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-09-28 15:16:09 +0530

    g

--- project_2_API/script.js
+++ project_2_API/script.js
@@ -1,42 +1,58 @@
-document.addEventListener('DOMContentLoaded', () => {
-const cityInput = document.getElementById("city-input")
-const getWeatherBtn = document.getElementById("get-weather-btn")
-const weatherInfo = document.getElementById("weather-info")
-const cityNameDisplay = document.getElementById("city-name")
-const tempratureDisplay = document.getElementById("temprature")
-const descriptionDispaly = document.getElementById("description")
-  const errorMessage = document.getElementById("error-message")
-  
- const API_KEY = "4f7dd6c3eb4e83eb30a80a1da0a2ec01"; //env variable
+document.addEventListener("DOMContentLoaded", () => {
+  const cityInput = document.getElementById("city-input");
+  const getWeatherBtn = document.getElementById("get-weather-btn");
+  const weatherInfo = document.getElementById("weather-info");
+  const cityNameDisplay = document.getElementById("city-name");
+  const temperatureDisplay = document.getElementById("temperature");
+  const descriptionDisplay = document.getElementById("description");
+  const errorMessage = document.getElementById("error-messag");
 
-   getWeatherBtn.addEventListener('click', async() => {
-  const city= cityInput.value.trim()
-     if (!city) return
-     
-     // itnmay throw an error
-     // server/database is also iin another continent
-     
-     try {
-     const weatherData = await fetchWeatherData(city)
-     displayWearther(weatherData)
-     } catch (error) {
-       showError()
-     }
-     
-     
-  })
-  
+  const API_KEY = "5f56d525d1619d0a2cd2eac4ce55588e"; //env variables
+
+  getWeatherBtn.addEventListener("click", async () => {
+    const city = cityInput.value.trim();
+    if (!city) return;
+
+    // it may throw an error
+    // server/database is always in another continent
+
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      showError();
+    }
+  });
+
   async function fetchWeatherData(city) {
-  //gets the data
+    //gets the data
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+
+    const response = await fetch(url);
+    console.log(typeof response);
+    console.log("RESPONSE", response);
+
+    if (!response.ok) {
+      throw new Error(" City Not found");
+    }
+    const data = await response.json();
+    return data;
   }
-  
-  function displayWearther(weatherData) {
-    //display
+
+  function displayWeatherData(data) {
+    console.log(data);
+    const { name, main, weather } = data;
+    cityNameDisplay.textContent = name;
+    temperatureDisplay.textContent = `Temperature : ${main.temp}`;
+    descriptionDisplay.textContent = `Weather : ${weather[0].description}`;
+
+    //unlock the display
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
   }
-  
+
   function showError() {
-  weatherInfo.classList.add('hidden')
-  errorMessage.classList.remove('hidden')
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
   }
-  
-})
+});
